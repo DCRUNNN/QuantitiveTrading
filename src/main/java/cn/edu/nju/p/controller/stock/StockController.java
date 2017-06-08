0b2323d404cedf9207acfe8f2daf8f9880b9134a
151250010 <151250010@smail.nju.edu.cn>
2017-06-09 00:18:32 +0800

    StockController内　增加判断是否存在股票方法

--- src/main/java/cn/edu/nju/p/controller/stock/StockController.java
+++ src/main/java/cn/edu/nju/p/controller/stock/StockController.java
@@ -2,9 +2,12 @@ package cn.edu.nju.p.controller.stock;
 
 import cn.edu.nju.p.baseresult.BaseResult;
 import cn.edu.nju.p.dao.StockDao;
+import cn.edu.nju.p.enums.ErrorCode;
 import cn.edu.nju.p.service.strategy.SectorService;
+import cn.edu.nju.p.utils.StockHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +25,16 @@ public class StockController {
     @GetMapping("/stock")
     public BaseResult getSectorStocks(@RequestParam String sector) {
         List<String> stocks = sectorService.getStocks(sector);
-        return new BaseResult<>(0,stocks);
+        return new BaseResult<>(0, stocks);
     }
 
+    @GetMapping("/check/{code}")
+    public BaseResult checkStock(@PathVariable String code) {
+        if (StockHelper.codeExists(code)) {
+            return new BaseResult<>(0, true);
+        } else {
+            return new BaseResult<>(ErrorCode.STOCK_NOT_FOUND.getErrorCode(), code + " not found!");
+        }
+    }
 }
+
